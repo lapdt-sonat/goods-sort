0117c195de598c76ac52c06eaf973389dac61fbf
lapdt <lapdt@sonat.vn>
2024-03-18 18:02:41 +0700

    feat: add model

--- assets/Scripts/ItemsPool.ts
+++ assets/Scripts/ItemsPool.ts
@@ -2,17 +2,19 @@ import { _decorator, Component, Node, Prefab } from "cc";
 const { ccclass, property } = _decorator;
 
 const itemIndexMap: Record<string, number> = {
-  soda_1: 0,
-  soda_2: 1,
-  coffee_1: 2,
-  juice_orange: 3,
-  juice_strawberry: 4,
-  pop_bottle_1: 5,
-  togo_cup_1: 6,
-  water_bottle_1: 7,
-  soda_3: 8,
-  juice_green: 9,
-  soda_4: 10,
+  teddy_white: 0,
+  teddy_blue: 1,
+  sheep: 2,
+  horse: 3,
+  elephant: 4,
+  duck: 5,
+  dragon: 6,
+  doll_2: 7,
+  doll_1: 8,
+  dinosaur: 9,
+  bunny_orange: 10,
+  avocado: 11,
+  alpaca: 12,
 };
 
 @ccclass("ItemsPool")
